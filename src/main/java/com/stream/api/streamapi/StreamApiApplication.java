6df31717af54618893c2414c8e97bb566bd585fb
Yogesh Yadav <119784141+mr-yogesh@users.noreply.github.com>
2023-01-09 03:16:02 +0530

    forEach, filter, map, collect

--- src/main/java/com/stream/api/streamapi/StreamApiApplication.java
+++ src/main/java/com/stream/api/streamapi/StreamApiApplication.java
@@ -2,6 +2,10 @@ package com.stream.api.streamapi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import javax.xml.transform.Source;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class StreamApiApplication {
 
 
@@ -17,29 +20,76 @@ public class StreamApiApplication {
 
 	static{
 		employees.add(
-			"Yogesh", "Yadav", 7000.0, List<E>.of("Project-1", "Project-2", "Project-3", "Projecy-4", "Project-5")
+			new Employee("Yogesh", "Yadav", 7000.0, List.of("Project-1", "Project-2", "Project-3", "Projecy-4", "Project-5"))
 		);
 
 		employees.add(
-			"Sumit", "Singh", 9000.0, List<E>.of("Project-6", "Project-7", "Project-8", "Projecy-9", "Project-0")
+			new Employee("Sumit", "Singh", 9000.0, List.of("Project-6", "Project-7", "Project-8", "Projecy-9", "Project-0"))
 		);
 
 		employees.add(
-			"Kamal", "Khan", 4000.0, List<E>.of("Project-11", "Project-12", "Project-13", "Projecy-14", "Project-15")
+			new Employee("Kamal", "Khan", 4000.0, List.of("Project-11", "Project-12", "Project-13", "Projecy-14", "Project-15"))
 		);
 
 		employees.add(
-			"Garry", "Sandhu", 7000.0, List<E>.of("Project-16", "Project-17", "Project-18", "Projecy-19", "Project-20")
+			new Employee("Garry", "Sandhu", 7000.0, List.of("Project-16", "Project-17", "Project-18", "Projecy-19", "Project-20"))
 		);
 	}
 	
 
 	public static void main(String[] args) {
-		SpringApplication.run(StreamApiApplication.class, args);
+		//SpringApplication.run(StreamApiApplication.class, args);
+
+		//Foreach
+		employees.stream()
+					.forEach(employee -> System.out.println(employee));
+
+					System.out.println();
+					System.out.println();
+
+		//Map
+		//Collect
+		//Suppose I want all employees salary increased by 150
+
+		List<Employee> incSalary = employees.stream()
+							.map( employee -> new Employee(
+								employee.getFirstName(),
+								employee.getLastName(),
+								employee.getSalary() + 150,
+								employee.getProjects()
+							)).collect(Collectors.toList());
+				
+		incSalary.stream()
+					.forEach(employee -> System.out.println(employee));
+
+					System.out.println();
+					System.out.println();
+
+		//Filter
+		List<Employee> filterSalary = 
+		employees.stream()
+					.filter(employee -> employee.getSalary() > 5000)
+					.map( employee -> new Employee(
+								employee.getFirstName(),
+								employee.getLastName(),
+								employee.getSalary() + 150,
+								employee.getProjects()
+							)).collect(Collectors.toList());
+
+		filterSalary.stream()
+						.forEach(employee -> System.out.println(employee));
+
+						System.out.println();
+						System.out.println();
+
+	
+
+
+
 	}
 
-	@GetMapping("/")
-	public String sayHello(){
-		return "Hello there I am from SpringBoot : (^///^)";
-	}
+	// @GetMapping("/")
+	// public String sayHello(){
+	// 	return "Hello there I am from SpringBoot : (^///^)";
+	// }
 }
